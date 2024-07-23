bcb8fa69c9430ec8eff83ca583e6e4ea9f4b9c69
aymenab45 <aymenababsa1@gmail.com>
2024-07-23 16:05:49 +0100

    option card

---
+++ lib/sections/option_section.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:travello/data/option_data.dart';
+import 'package:travello/widgets/option_card_widget.dart';
+
+class OptionSection extends StatelessWidget {
+  const OptionSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20, left: 17, right: 17),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: optionData
+            .map((option) => OptionCardWidget(
+                name: option.optionName,
+                color: option.optionColor,
+                icon: option.optionIcon))
+            .toList(),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/option_card_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class OptionCardWidget extends StatelessWidget {
+  final String name;
+  final Color color;
+  final IconData icon;
+  const OptionCardWidget(
+      {super.key, required this.name, required this.color, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90,
+      width: 75,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: const [
+            BoxShadow(
+                color: Color.fromARGB(57, 0, 0, 0),
+                blurRadius: 8,
+                spreadRadius: 0.5,
+                offset: Offset(-2, 2),
+                blurStyle: BlurStyle.outer),
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [_buildOpionIcon(), _buildOptionName()],
+      ),
+    );
+  }
+
+  Widget _buildOpionIcon() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 1),
+      child: Icon(
+        icon,
+        color: color,
+      ),
+    );
+  }
+
+  Widget _buildOptionName() {
+    return Text(
+      name,
+      style: TextStyle(color: color, fontWeight: FontWeight.bold),
+    );
+  }
+}
